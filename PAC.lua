4e1a6f786edc3fcc36559f49d33a51c1a25c73c1
Va-Dim-tech <kozevnikovvadim51@gmail.com>
2026-07-23 17:55:51 +0700

    initialization

--- PAC.lua
+++ PAC.lua
@@ -1,58 +1,37 @@
-a = component.list('transposer')
-if a == nil or pairs(a)(a) == nil then 
-	error('нет транспозера')
-end
-tr = component.proxy(pairs(a)(a))
-inside = 6 -- выход с пресса и интерфейса
-ouside = 6 -- интерфейс
-fbside = 6 -- низ пресса
-ffside = 6 -- перед пресса
-ee = component.proxy(component.list('eeprom')())
-text = ee.getLabel()
-if string.sub(text, 1, 3) == 'PAS' and #text >= 7 then
---reading
-inside = tonumber(string.sub(text, 4, 4))
-ouside = tonumber(string.sub(text, 5, 5))
-fbside = tonumber(string.sub(text, 6, 6))
-ffside = tonumber(string.sub(text, 7, 7))
-else
--- colibration
--- redstone factory front
--- press factory out
-for i=2, 5 do
-	nam = tr.getInventoryName(i)
-	if nam ~= nil then
-		if nam == 'appliedenergistics2:interface' then
-			ouside = i
-		else
-			siz = tr.getInventorySize(i)
-			flag = false
-			for j=1, siz do
-				a = tr.getStackInSlot(i, j)
-				if a ~= nil then
-					if a.name == 'appliedenergistics2:material' then
-						flag = true
-						inside = i
-						break
-					elseif a.name == 'minecraft:redstone' then 
-						ffside = i
-						flag = true
-						break
-					end
-				end
-			end
-			if flag == false then
-				fbside = i
+
+tp = nil
+mn = nil
+bt = nil
+is = nil
+ss = nil
+for tr in pairs(component.list("transposer")) do
+	for i=0, 5 do
+		a = component.invoke(tr, "getInventoryName", i)
+		if a == 'appliedenergistics2:interface' then
+			mn = component.proxy(tr)
+		end
+		if a == 'appliedenergistics2:inscriber' then
+			if i == 0 then
+				tp = component.proxy(tr)
+			elseif i == 1 then
+				bt = component.proxy(tr)
 			end
 		end
 	end
 end
-if inside == 6 or ouside == 6 or fbside == 6 or ffside == 6 then
-	computer.beep('-..')
-	error('cant colibrate')
+
+for i=0, 1 do
+	for j, k in pairs(mn.getAllStacks(i).getAll()) do
+		if k.name == 'appliedenergistics2:material' then -- todo add check for press
+			is = i
+		end
+	end
+	if is == nil then
+	ss = i 
+	end
 end
-ee.setLabel(string.format('PAS%s%s%s%s', inside, ouside, fbside, ffside))
-end
+
+error(tp.address .. " " .. mn.address .. " " .. bt.address .. " " .. is .. " " .. ss)
 function typ(nm, dm)
 	if nm == 'minecraft:air' then
 	return 17
