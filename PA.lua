6f03da9258db7e31e4d4e5b925f2b0813b7aa36f
Va-Dim-tech <kozevnikovvadim51@gmail.com>
2026-07-26 17:42:05 +0700

    creating compact version

--- PA.lua
+++ PA.lua
@@ -1,76 +1,10 @@
-a = component.list('transposer')
-if a == nil or pairs(a)(a) == nil then 
-error('íåò òğàíñïîçåğà')
-end
-tr=component.proxy(pairs(a)(a))
-inside=6
-ouside=6
-fbside=6
-ffside=6
-ee=component.proxy(component.list('eeprom')())
-text=ee.getLabel()
-if string.sub(text,1,3) == 'PAS' and #text >= 7 then
---reading
-inside=tonumber(string.sub(text,4,4))
-ouside=tonumber(string.sub(text,5,5))
-fbside=tonumber(string.sub(text,6,6))
-ffside=tonumber(string.sub(text,7,7))
-else
--- colibration
--- redstone factory front
--- press factory out
-for i=2,5 do
-nam=tr.getInventoryName(i)
-if nam ~= nil then
-if nam == 'appliedenergistics2:interface' then
-ouside=i
-else
-siz=tr.getInventorySize(i)
-flag=false
-for j=1,siz do
-a=tr.getStackInSlot(i,j)
-if a ~= nil then
-if a.name == 'appliedenergistics2:material' then
-flag=true
-inside=i
-break
-elseif a.name == 'minecraft:redstone' then 
-ffside=i
-flag=true
-break
-end
-end
-end
-if flag == false then
-fbside=i
-end
-end
-end
-end
-if inside == 6 or ouside == 6 or fbside == 6 or ffside == 6 then
-computer.beep('-..')
-error('cant colibrate')
-end
-ee.setLabel(string.format('PAS%s%s%s%s',inside,ouside,fbside,ffside))
-end
-function typ(nm,dm)
-if nm == 'minecraft:air' then
-return 17
-end
-for t1,t2 in pairs(items) do
-if t1.name == nm and t1.damage == dm then
-return t2
-end
-end
-return 0
-end
 a='appliedenergistics2:material'
 b='damage'
 c='name'
-items={[{[b]=19,[c]=a}]=1,--êğåì
-[{[b]=15,[c]=a}]=2,-- ëîã
-[{[b]=13,[c]=a}]=3,-- âû÷
-[{[b]=14,[c]=a}]=4,-- èíæ
+items={[{[b]=19,[c]=a}]=1,
+[{[b]=15,[c]=a}]=2,
+[{[b]=13,[c]=a}]=3,
+[{[b]=14,[c]=a}]=4,
 [{[b]=5,[c]=a}]=5,
 [{[b]=2,[c]='galacticraftcore:basic_item'}]=5,
 [{[b]=0,[c]='minecraft:gold_ingot'}]=6,
@@ -78,10 +12,10 @@ items={[{[b]=19,[c]=a}]=1,--êğåì
 [{[b]=0,[c]='minecraft:diamond'}]=8,
 [{[b]=19,[c]='ic2:crafting'}]=8,
 [{[b]=0,[c]='minecraft:redstone'}]=9,
-[{[b]=20,[c]=a}]=10,-- êğåì
-[{[b]=18,[c]=a}]=11,-- ëîã
-[{[b]=17,[c]=a}]=12,-- èíæ
-[{[b]=16,[c]=a}]=13,-- âû÷
+[{[b]=20,[c]=a}]=10,
+[{[b]=18,[c]=a}]=11,
+[{[b]=17,[c]=a}]=12,
+[{[b]=16,[c]=a}]=13,
 [{[b]=24,[c]=a}]=14,
 [{[b]=22,[c]=a}]=15,
 [{[b]=23,[c]=a}]=16
@@ -95,124 +29,172 @@ rec={
 {11,9,10,15},
 {13,9,10,16}
 }
-dat=tr.getAllStacks(inside).getAll()
-for i=1,#dat do
-a=typ(dat[i].name,dat[i].damage)
-if a >= 1 and a <= 4 then
-tr.transferItem(inside,inside,64,i,#dat - (a - 1))
-elseif a == 0 then 
-tr.transferItem(inside,ouside,64,i)
+tp=nil
+mn=nil
+bt=nil
+is=nil
+ss=nil
+its=nil
+mi=nil
+to=nil
+bo=nil
+function typ(it)
+if it==nil or it.name=='minecraft:air' then
+return 17
+end
+for l1,l2 in pairs(items) do
+if l1.name==it.name and l1.damage==it.damage then
+return l2
 end
 end
-dat=nil
-state=1
+return 0
+end
+for tr in pairs(component.list("transposer")) do
+for i=0,5 do
+t=component.proxy(tr)
+a=t.getInventoryName(i)
+if a=='appliedenergistics2:interface' then
+mn=t
+its=i
+end
+if a=='appliedenergistics2:inscriber' then
+if i==0 then
+tp=t
+elseif i==1 then
+bt=t
+else
+mi=i 
+end
+end
+a=t.getInventorySize(i)
+if a and a > 0 then 
+if tp==t then 
+to=i
+elseif bt==t then
+bo=i
+end
+end
+end
+end
+function tra(s1,t1,s2,s3,t2,s4,i1,i2)
+f=1
+if s1==s2 then
+f=i1
+else
+if s3==s4 then
+f=i2
+end
+if f then 
+t1.transferItem(s1,s2,1,i1,f)
+else
+t1.transferItem(s1,s2,1,i1)
+end 
+end
+if s3 ~=s4 then
+if i2 then 
+t2.transferItem(s3,s4,1,f,i2)
+else
+t2.transferItem(s3,s4,1,f)
+end
+end
+end
+ss=1
+for i=0,1 do
+dat=mn.getAllStacks(i).getAll()
+for j,k in pairs(dat) do
+a=typ(k)
+if a >=1 and a <=4 then 
+is=i
+mn.transferItem(i,i,64,j,#dat -(a -1))
+elseif a==0 then
+mn.transferItem(i,its,64,j,1)
+end
+end
+if not is then
+ss=0
+end
+end
+tra(0,tp,to,1,mn,is,1,nil)
+tra(1,bt,bo,0,mn,is,1,nil)
+mn.transferItem(mi,is,nil,1)
+if not (tp and mn and bt and its and mi and ss ~=is) then 
+error('no press ' .. mi .. " " .. is .. " " .. its .. " " .. ss)
+end
+computer.beep(900,0.1)
+prcd=-1
+tcprs=17
 tecrec=0
-pdat=nil
-c=false
-
-function st1()
-dat=tr.getAllStacks(inside).getAll()
-ms={[17]=true}
-b=false
-it=false
-for i=1,#dat do
-a=typ(dat[i].name,dat[i].damage)
-if a ~= 17 then
-if a == 0 or (c and a > 4)then
-tr.transferItem(inside,ouside,64,i)
-else
-if a > 4 then
-it=true
+ppres=0
+prms={}
+function proc()
+dat=mn.getAllStacks(is).getAll()
+ms={[17]=1,[ppres]=1}
+chg=false
+for i,j in pairs(dat) do
+a=typ(j)
+if a ~=17 then
+if a==0 then
+mn.transferItem(is,its,64,i)
+elseif a >=1 and a <=4 then
+if #dat -(a -1) ~=i then
+mn.transferItem(is,is,64,i,#dat -(a -1))
 end
-ms[a]=true
-if pdat ~= nil then
-if pdat[i].name ~= dat[i].name or pdat[i].damage ~= dat[i].damage then
-b=true
-end
+ms[a]=#dat -(a -1) 
 else 
-b=true
+ms[a]=i
+if (prms[a]==nil) ~=(ms[a]==nil) then
+chg=true
 end
 end
 end
 end
-if c then
-c=false
-return
+prms=ms
+for i,j in pairs(rec) do
+if ms[j[1]] and ms[j[2]] and ms[j[3]] then
+if tecrec==i or prcd==0 then
+if prcd==0 then
+a=typ(tp.getStackInSlot(0,1))
+if a ~=17 and a ~=j[1] then
+tra(0,tp,to,1,mn,is,1,nil)
+ppres=0
 end
-if it == false then 
-return
+a=typ(bt.getStackInSlot(1,1))
+if a ~=17 and a ~=j[1] then
+tra(1,bt,bo,0,mn,is,1,nil)
 end
-pdat=dat
-for i=1,#rec do
-if ms[rec[i][1]] and ms[rec[i][2]] and ms[rec[i][3]] then
+end
+if j[1] ~=17 then
+tra(is,mn,1,to,tp,0,ms[j[1]],nil)
+if j[1] > 0 and j[1] < 5 then
+ppres=j[1]
+end
+end
+if j[3] ~=17 then
+tra(is,mn,0,bo,bt,1,ms[j[3]],nil)
+end
+if j[2] ~=17 then
+mn.transferItem(is,mi,1,ms[j[2]],1)
+end
+prcd=prcd+1
 tecrec=i
-if b then
-computer.pullSignal(0.5)
-else
-state=2
-end
 return
 end
 end
-if b then
-computer.pullSignal(3)
-else
-c=true
+end
+if not chg then
+computer.pullSignal(1)
 end
 end
-
-while true do
-computer.pullSignal(0)
-if state == 1 then 
-st1()
-elseif state == 2 then
-a1=true
-a2=true
-a3=true
-r=5
-e=-1
-e1=-1
-while e1 == e do
-e=0
-dat=tr.getAllStacks(inside).getAll()
-for i=1,#dat do
-a=typ(dat[i].name,dat[i].damage)
-if rec[tecrec][1] == a and a1 then
-a1=false
-b=tr.getStackInSlot(0,1)
-if b ~= nil then
-c=typ(b.name,b.damage)
-if c ~= a then
-if c ~= 0 and c <= 4 then
-tr.transferItem(0,inside,64,1,#dat - (c - 1))
-else
-tr.transferItem(0,inside,64,1)
+while true do 
+computer.pullSignal(0.7)
+if prcd ~=0 then
+a=typ(mn.getStackInSlot(mi,2))
+if a ~=17 then
+prcd=prcd -mn.transferItem(mi,its,nil,2)
+end
+if prcd < 0 then 
+prcd=0
 end
 end
-end
-tr.transferItem(inside,0,1,i)
-r=a
-elseif rec[tecrec][2] == a and a2 then
-a2=false
-tr.transferItem(inside,ffside,1,i)
-elseif rec[tecrec][3] == a and a3 then
-a3=false
-tr.transferItem(inside,fbside,1,i)
-elseif rec[tecrec][4] == a then
-e=e + tr.getSlotStackSize(inside,i)
-s=i
-end
-end
-if e1 == -1 then
-e1=e
-end
-end
-tr.transferItem(inside,ouside,1,s)
-if r < 5 then 
-tr.transferItem(0,inside,1,1,#dat - (r - 1))
-else
-tr.transferItem(0,inside,1,1)
-end
-state=1
-end
-end
+proc()
+end 
